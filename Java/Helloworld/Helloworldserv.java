964338503be4de772f298b1d808c4e99bc33b1ef
Chirag Girdhar <chirag.girdhar@spit.ac.in>
2024-01-04 00:53:54 +0530

    Fourth commit

--- Java/Helloworld/Helloworldserv.java
+++ Java/Helloworld/Helloworldserv.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorldServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    public HelloWorldServlet() {
+    public Helloworldserv() {
         super();
     }
 
